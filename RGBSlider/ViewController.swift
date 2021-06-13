842df9b9dbd94efcf9ac8ca0f4af36f8dcfdb6b8
Alexander Bikteev <bikteev@mail.ru>
2021-06-13 13:09:26 +0900

    1. View change color 2. Sliders change color view RGB complete

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var palette: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        palette.layer.cornerRadius = 10
+        
+        redSlider.minimumValue = 1
+        redSlider.maximumValue = 255
+        greenSlider.minimumValue = 1
+        greenSlider.maximumValue = 255
+        blueSlider.minimumValue = 1
+        blueSlider.maximumValue = 255
     }
 
+    @IBAction func redSliderAction() {
+        palette.backgroundColor = UIColor(_colorLiteralRed: redSlider.value/255, green: greenSlider.value/255, blue: blueSlider.value/255, alpha: 1)
+        redLabel.text = String(Int(redSlider.value))
+    }
+    @IBAction func greenSliderAction() {
+        palette.backgroundColor = UIColor(_colorLiteralRed: redSlider.value/255, green: greenSlider.value/255, blue: blueSlider.value/255, alpha: 1)
+        greenLabel.text = String(Int(greenSlider.value))
+    }
+    @IBAction func blueSliderAction() {
+        palette.backgroundColor = UIColor(_colorLiteralRed: redSlider.value/255, green: greenSlider.value/255, blue: blueSlider.value/255, alpha: 1)
+        blueLabel.text = String(Int(blueSlider.value))
+    }
+    
 
+    
 }
-
+/*
+extension UIColor {
+    convenience init(_ r: Float,_ g: Float,_ b: Float,_ a: Float) {
+        self.init(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: 1)
+    }
+}
+*/
