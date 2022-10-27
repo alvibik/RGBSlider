6c1d83afb86ebc35fbd52c9b823e2701fd9a6220
Александр Биктеев <bikteev@mail.ru>
2022-10-27 11:24:49 +0600

    Setup logic

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -11,17 +11,33 @@ class ViewController: UIViewController {
 
 
     @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var rgbView: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rgbView.layer.cornerRadius = 20
     }
 
 
     @IBAction func setRedSlider() {
         redLabel.text = "\(round(redSlider.value * 100) / 100)"
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func setGreenSlider() {
+        greenLabel.text = "\(round(greenSlider.value * 100) / 100)"
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func setBlueSlider() {
+        blueLabel.text = "\(round(blueSlider.value * 100) / 100)"
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
 }
